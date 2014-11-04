f5c48fe4ddd9675cd60fec08a74a8df3e11980f9
fulgurio <v.guerard@fulgurio.net>
2014-11-04 15:45:36 +0100

    Adding Twig extension

---
+++ Extension/TwigExtension.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * This file is part of the LightCMSConfigBundle package.
+ *
+ * (c) Fulgurio <http://fulgurio.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fulgurio\LightCMSConfigBundle\Extension;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+class TwigExtension extends \Twig_Extension
+{
+    /**
+     * Doctrine component
+     * @var Doctrine\Bundle\DoctrineBundle\Registry
+     */
+    private $doctrine;
+
+
+    /**
+     *
+     * @param Doctrine\Bundle\DoctrineBundle\Registry $doctrine
+     */
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Twig_Extension::getFunctions()
+     */
+    public function getFunctions()
+    {
+        return array(
+            'getConfig' => new \Twig_Function_Method($this, 'getConfig'),
+        );
+    }
+
+    /**
+     * Get config value
+     *
+     * @param string $metaKey
+     * @return string
+     */
+    public function getConfig($metaKey)
+    {
+        $config = $this->doctrine->getRepository('FulgurioLightCMSConfigBundle:Config')->findOneBy(array('meta_key' => $metaKey));
+        if ($config)
+        {
+            return $config->getMetaValue();
+        }
+        return NULL;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Twig_ExtensionInterface::getName()
+     */
+    public function getName()
+    {
+        return 'LightCMSConfig_extension';
+    }
+}
